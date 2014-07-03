4b78a7d86e18639ec453df9f53a7a7c777542b20
Henok <henok@Hannas-MacBook-Pro.local>
2014-07-03 16:00:20 -0400

    Changed email for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,13 +32,13 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://cryptic-garden-2639.heroku.com/' }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.default_url_options = { :host => 'http://cryptic-garden-2639.heroku.com/' }
 config.action_mailer.raise_delivery_errors = true
 
 
