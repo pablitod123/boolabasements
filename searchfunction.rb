d0290e93d334e7beb146c9c6884fa9c292387df2
Paul Styslinger <pablitod123@gmail.com>
2014-07-07 09:36:02 -0400

    commited

---
+++ searchfunction.rb
@@ -0,0 +1,11 @@
+# Retrieves user request from database
+# converts it to jd
+# Compares user request dates to all listings for matches
+# stores matches in array and puts them into the view. 
+
+
+usersearch1 = current_user.last.datein
+usersearch2 = current_user.last.dateout
+
+
+
