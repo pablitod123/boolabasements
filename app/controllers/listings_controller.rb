1fde3491dcd356872b4b86ba41c0124abf15e909
Henok <henok@Hannas-MacBook-Pro.local>
2014-07-08 10:43:09 -0400

    Fixing edit and delete

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,11 +11,21 @@ class ListingsController < ApplicationController
 		redirect_to '/listings'
 	end
 
+	def edit
+		@listing = Listing.find(params[:id])
+	end
+
 	def index
 		@listings = current_user.listings.all.reverse
 	end
 
 	def listing_params
 		params.require(:listing).permit(:datein, :dateout, :location, :user_id)
+	end
+
+	def destroy
+		Listing.find(params[:id]).destroy
+		flash[:success] = "Congrats Dude!"
+		redirect_to '/listings'
 	end
 end
